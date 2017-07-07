4bb1c6d623d58638ab98a44d5652506f4fe83448
tandisunarto <tandi.sunarto@hotmail.com>
2017-07-06 22:44:36 -0400

    mini refactor

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import TestButton from './Button';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<TestButton />, document.getElementById('test-button'));
 registerServiceWorker();
