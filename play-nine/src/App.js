4cac251fd2e1524a9662bdbd9f0e903b06d5b9f0
tandisunarto <tandi.sunarto@hotmail.com>
2017-07-28 17:52:41 -0400

    initial play-nine commit

--- play-nine/src/App.js
+++ play-nine/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TestButton from './Button';
 
 class App extends Component {
   render() {
@@ -9,8 +8,11 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Play Nine</h2>
+          <h2>PLay Nine</h2>
         </div>
+        <p className="App-intro">
+          {/* To get started, edit <code>src/App.js</code> and save to reload. */}
+        </p>
       </div>
     );
   }
