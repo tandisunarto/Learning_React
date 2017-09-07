c9a81d5f6497689e158bd30392e7f8dd8288bd46
tandisunarto <tandi.sunarto@hotmail.com>
2017-09-07 16:41:24 -0400

    complete mocking up the UI

--- time_tracking_app/public/js/timers/editable-timer/editable-timer.js
+++ time_tracking_app/public/js/timers/editable-timer/editable-timer.js
@@ -1,8 +1,29 @@
+class EditableTimer extends React.Component {
+
+    render() {
+        const timer = this.props.timers;
+        if (timer.editFormOpen) {
+            return (
+                <TimerForm
+                    timer={timer} />
+            )
+        } else {
+            return (
+                <Timer
+                    timer={timer} />
+            )
+        }
+
+    }
+}
+
 class TimerForm extends React.Component {
     render() {
-        const timer = this.props.timer;
-        console.log(timer);
-        const submitText = (timer !== 'undefined' && timer.title) ? 'Update' : 'Create';
+         var timer = this.props.timer;
+         if (timer === undefined)
+             timer = { title: null, project: null }
+
+        const submitText = timer.title ? 'Update' : 'Create';
         return (
             <div className='ui centered card'>
                 <div className='content'>
@@ -32,29 +53,35 @@ class TimerForm extends React.Component {
 
 class Timer extends React.Component {
     render() {
-        console.log(this.props)
+        var timer = this.props.timer;
+        const elapsedString = helpers.renderElapsedString(timer.elapsed);
         return (
-            <div>Timer</div>
-        )
-    }
-}
-
-class EditableTimer extends React.Component {    
-        
-    render() {
-        const timer = this.props.timers;
-
-        if (timer.editFormOpen) {
-            return (
-                <TimerForm
-                    timer={timer} />
-            )
-        } else {
-            return (
-                <Timer
-                    timer={timer} />
-            )
-        }
-
+            <div className='ui centered card'>
+                <div className='content'>
+                    <div className='header'>
+                        {timer.title}
+                    </div>
+                    <div className='meta'>
+                        {timer.project}
+                    </div>
+                    <div className='center aligned description'>
+                        <h2>
+                            {elapsedString}
+                        </h2>
+                    </div>
+                    <div className='extra content'>
+                        <span className='right floated edit icon'>
+                            <i className='edit icon' />
+                        </span>
+                        <span className='right floated trash icon'>
+                            <i className='trash icon' />
+                        </span>
+                    </div>
+                </div>
+                <div className='ui bottom attached blue basic button'>
+                    Start
+                </div>
+            </div>
+        );
     }
 }
