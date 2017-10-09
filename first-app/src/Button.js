6c1dfa3b60515668c9247810d9b4b65f1330ef85
tandisunarto <tandi.sunarto@hotmail.com>
2017-10-09 16:42:26 -0400

    passing function to setState

--- first-app/src/Button.js
+++ first-app/src/Button.js
@@ -1,10 +1,44 @@
 import React from 'react';
 
+class TestButton extends React.Component {
 
-const TestButton = () => {    
-    return (
-        <button>Function Com Button</button>
-    );
+    state = {
+        counter: 0
+    }
+
+    increment = () => {
+        this.setState(
+            (prevState) => {
+                return {
+                    counter: prevState.counter + 1
+                }
+            }
+        )
+    }
+
+    decrement = () => {
+        this.setState(
+            (prevState) => {
+                return {
+                    counter: prevState.counter > 0 ? prevState.counter - 1 : 0
+                }
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <label className="Red-Font">{this.state.counter}</label>
+                </div>
+                <div>
+                    <button onClick={this.increment}>+</button>
+                    <button onClick={this.decrement}>-</button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default TestButton;
