b017a7649fc53e968d09a4737bab8a99e4c1c640
Sunarto <tsunarto@icf2008831.icfi.icfconsulting.com>
2018-09-18 09:51:34 -0400

    first apollo lesson

--- crm-apollo/src/App.js
+++ crm-apollo/src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Contacts from './Contacts';
-import { ApolloClient, ApolloProvider } from 'react-apollo';
+import { ApolloProvider, Query } from 'react-apollo'; 
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
 
-const client = new ApolloClient();
+const client = new ApolloClient({
+  uri: "https://api-useast.graphcms.com/v1/cjm6qmvew02p601c4jqk7dtmy/master"
+});
+
+const POST_QUERY = gql`
+{
+  posts {
+    id
+    title
+    body
+  }
+}
+`;
+
+// client.query({
+//   query: testQuery
+// }).then(res => console.log(res));
 
 class App extends Component {
   render() {
@@ -14,7 +32,13 @@ class App extends Component {
           <header className="App-header">
             <h1 className="App-title">CRM</h1>
           </header>
-          <Contacts />
+          <Query query={POST_QUERY}>
+            {({loading, data}) => {
+              if (loading) return "Loading...";
+              const { posts } = data;
+              return posts.map(post => <h1>{post.title}</h1>)
+            }}
+          </Query>
         </div>
       </ApolloProvider>
     );
