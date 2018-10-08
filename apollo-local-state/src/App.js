32e06d9037bf939dbf46f97e6267417a265f1b3b
Sunarto <tsunarto@icf2008831.icfi.icfconsulting.com>
2018-10-08 12:53:27 -0400

    Add menu links

--- apollo-local-state/src/App.js
+++ apollo-local-state/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import AuthStatusCompose from './AuthStatusCompose';
 import AuthStatusQuery from './AuthStatusQuery';
@@ -12,6 +12,12 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <ul>
+          <li><Link to={'/auth'}>Read/Mutate Local state</Link></li>
+          <li><Link to={'/query'}>Read local/remote state using query component</Link></li>
+          <li><Link to={'/compose'}>Read local/remote state using compose</Link></li>
+          <li><Link to={'/addbook'}>Mutation(add) book in graph cool</Link></li>
+        </ul>
         <Switch>
           <Route path="/compose" component={AuthStatusCompose} />
           <Route path="/query" component={AuthStatusQuery} />
