3fb101d34871cd999393de1c2d51a0c578ae019d
tandisunarto <tandi.sunarto@hotmail.com>
2019-11-20 17:26:54 -0500

    Try something different on useState

--- hooks/src/components/CounterHook.jsx
+++ hooks/src/components/CounterHook.jsx
@@ -9,14 +9,32 @@ const CounterHook = () => {
 
   const [state, incrementCount] = useState(defaultState);
 
-  const handleClick = () => ({
+  const handleClick1 = () => ({
     ...state,
     count: state.count + 1
   });
 
+  const handleClick2 = () => {
+    incrementCount({
+      ...state,
+      count: state.count + 1
+    });
+  }
+
   return (
-    <button onClick={() => incrementCount(handleClick())}> {state.name} Count {state.count}</button>
-  )
+    <div>
+      <div>
+        <button onClick={() => incrementCount(handleClick1())}>          
+          {state.name} Count_1 {state.count}
+        </button>
+      </div>
+      <div>
+        <button onClick={handleClick2}>
+          {state.name} Count_2 {state.count}
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default CounterHook;
