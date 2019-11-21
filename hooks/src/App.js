855a573af4968823d7d9e34fc94bda1c645b4d6c
tandisunarto <tandi.sunarto@hotmail.com>
2019-11-21 13:38:03 -0500

    useState with default value object

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import CounterClass from './components/CounterClass';
 import CounterHook from './components/CounterHook';
 import CounterHookTwo from './components/CounterHookTwo';
+import NameObjectHook from './components/NameObjectHook';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <CounterClass />
       <CounterHook />
       <CounterHookTwo />
+      <NameObjectHook />
     </div>
   );
 }
---
+++ hooks/src/components/NameObjectHook.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const NameObjectHook = () => {
+
+  const [name, setName] = useState({firstName:"", lastName: ""});
+
+  const handleChange = e => {
+    console.log(e.target.name,'=',e.target.value)
+    setName({
+      ...name,
+      [e.target.name]: e.target.value      
+    });
+  }
+
+  return (
+    <form>
+      <input
+        type="text"
+        value={name.firstName}
+        name="firstName"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        value={name.lastName}
+        name="lastName"
+        onChange={handleChange}
+      />
+      <h2>First Name: {name.firstName}</h2>
+      <h2>Last Name: {name.lastName}</h2>
+    </form>
+  );
+}
+
+export default NameObjectHook;
